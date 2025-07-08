d1dbe5c495df76879d06c608ef0279c0fc5a2b83
Ahsan Mahmood <aoneahsan@gmail.com>
2025-07-08 15:48:44 +0500

    capacitor init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zaions.biometric_auth',
+  appName: '@capacitor-community/biometric-auth',
+  webDir: 'build',
+};
+
+export default config;
